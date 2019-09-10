4fb6d7f806da103351153ad22b86265858bb2d5a
Rav-T <hiddenkrypt@gmail.com>
2019-09-09 23:05:43 -0700

    cleanup of former state file project data

--- lib/gamegent_state.h
+++ lib/gamegent_state.h
@@ -5,7 +5,7 @@
  * a selection of states the overall program can be in. Different subsystems may change the program state
  */
 typedef enum {
-    state_main_menu /**< Program is currently in the menu. Emulation is paused. */
+    state_main_menu /**< Program is currently in the main menu. Emulation is paused. (starting condition)*/
     //state_emulating /**< Menu is hidden, emulation is proceeding */
 } gamegent_state;
 
