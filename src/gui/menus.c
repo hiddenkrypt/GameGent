36a04ed86f67546305ac1d70ba1b4f5ca1225256
Rav-T <hiddenkrypt@gmail.com>
2019-09-10 22:43:34 -0700

    Displays the main menu! no functionality, but it looks good!

--- src/gui/menus.c
+++ src/gui/menus.c
@@ -3,24 +3,48 @@
 #include "gamegent_state.h"
 #include "tilemap.h"
 #include "menus.h"
+#include "main.h"
 
-
+static int menuIndex = 0;
+void drawMenu( SDL_Renderer*  renderer );
 void Menus_draw(SDL_Renderer*  renderer, gamegent_state currentState){
+    char menuTitle[4][11] = {
+        { 148, 159, 159, 159, 159, 159, 159, 159, 159, 149, 0 },
+        { 158, 144, 145, 'a', 'm', 'e',  32, 146, 147, 158, '\0' },
+        { 158, 152, 153, 'e', 'n', 't',  32, 154, 155, 158, '\0' },
+        { 156, 159, 159, 159, 159, 159, 159, 159, 159, 157, 0 }
+    };
+    tiles_paintStringAt(10-5, 0, menuTitle[0],  renderer );
+    tiles_paintStringAt(10-5, 1, menuTitle[1],  renderer );
+    tiles_paintStringAt(10-5, 2, menuTitle[2],  renderer );
+    tiles_paintStringAt(10-5, 3, menuTitle[3],  renderer );
     switch( currentState ){
         case state_main_menu:
-            drawMainMenu( renderer );
+            drawMenu( renderer );
             break;
         default: break; //noop
     }
 }
-
-void drawMainMenu( SDL_Renderer*  renderer ){
-    char menuStringLine1[8] = { 144, 145, 'a', 'm', 'e', 146, 147, '\0' };
-    char menuStringLine2[8] = { 152, 153, 'e', 'n', 't', 154, 155, '\0' };
-    tiles_paintStringAt(10-3, 1, menuStringLine1,  renderer );
-    tiles_paintStringAt(10-3, 2, menuStringLine2,  renderer );
-    //tiles_paintCharAt(10-3,1,255,renderer);
-    //draw title first
-    //then each item in a row
+const char mainMenuItemStrings[7][20]= {
+    "Continue",
+    "Load ROM...",
+    "Load State",
+    "Save State",
+    "State Number:",
+    "Options...",
+    "EXIT"
+};
+void drawMenu( SDL_Renderer*  renderer ){
+    for(int i = 0; i < 7; i++){
+        tiles_paintStringAt(1, 10+i, mainMenuItemStrings[i], renderer );
+    }
+    //paint background of selected menuitem
+    //draw each item in a row
 }
 
+
+
+
+void MainMenu_Exit(){
+    shutdown();
+}
