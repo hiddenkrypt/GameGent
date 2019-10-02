8c0d53399d49f3935f1c7e68d96182f2e8ed4cd2
Rav-T <hiddenkrypt@gmail.com>
2019-10-02 16:29:51 -0700

    documentation update

--- src/dmg/cpu/cpu.c
+++ src/dmg/cpu/cpu.c
@@ -3,6 +3,10 @@
 
 DmgRegisters registers;
 
+/** \brief sets up CPU initial state
+ * configures internal registers to initial state, serving as a CPU restart option
+ * \todo pick state based on presence of bootloader rom.
+ */
 void CPU_init(){ //serves as a restart
     registers.af = 0x0000;
     registers.bc = 0x0000;
--- src/dmg/dmg.c
+++ src/dmg/dmg.c
@@ -3,12 +3,49 @@
 #include "dmg.h"
 #include "cpu.h"
 
+static bool emulating = false;
+
+/** \brief startup emulation core
+ * initializes DMG sub components and prepares emulation
+ */
 void DMG_init(){
     CPU_init();
 }
+
+/** \brief loads a game cartridge into the DMG
+ * fetches rom information, parses rom metadata, and configures DMG appropriately.
+ * \param path filepath to find the game rom.
+ */
 void DMG_LoadRom(char* path){}
-void DMG_startEmulation(){}
-void DMG_haltEmulation(){}
+
+
+/** sets emulating to true, causing the DMG to emulate a processor clock cycle when given program control in DMG_tick() */
+void DMG_startEmulation(){
+    emulating = true;
+}
+/** sets emulating to false, causing the DMG to do nothing when given program control in DMG_tick() */
+void DMG_haltEmulation(){
+    emulating = false;
+}
+
+/** \brief returns state of the emulating variable
+ * \return boolean value: emulating
+ */
 bool DMG_isEmulating(){
-    return false;
+    return emulating;
+}
+
+/** \brief gives the DMG some time to execute emulation
+ *
+ *  passes program control to the DMG emulation as a part of the Main Program Loop
+ * if emulating is FALSE, this does nothing but return.
+ * \todo do anything
+ * \todo add check for loaded rom
+ * \todo pause for DMG clock rate management.
+ * \return bool for whether the DMG is currently emulating
+ */
+
+bool DMG_tick(){
+    if (!emulating){ return false; }
+    return true;
 }
