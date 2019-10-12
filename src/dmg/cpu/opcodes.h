f5fa197ba42a15660cd671552fd38fc1571fa2e1
Rav-T <hiddenkrypt@gmail.com>
2019-10-11 23:42:45 -0700

    added descriptions for the first 16 op codes

--- src/dmg/cpu/opcodes.h
+++ src/dmg/cpu/opcodes.h
@@ -1,12 +1,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 #pragma once
-#define NO_FLAGS {UNAFFECTED,UNAFFECTED,UNAFFECTED,UNAFFECTED}
+#define NO_FLAGS E_UNAFFECTED,E_UNAFFECTED,E_UNAFFECTED,E_UNAFFECTED
 typedef enum flagEffectEnum{
-    UNAFFECTED = 0,
-    FLAG_CLEARED = 1,
-    FLAG_RAISED = 2,
-    FLAG_APPLIED = 3
+    E_UNAFFECTED = 0,
+    E_CLEARED = 1,
+    E_RAISED = 2,
+    E_APPLIED = 3
 } flagEffect;
 
 typedef struct{
@@ -26,9 +26,25 @@ typedef struct{
 } instruction;
 
 static instruction codeTable[ 0xFF ] = {
-    { 0x00, 0, 4, NO_FLAGS, "NOP" },
-    { 0x01, 2, 12, NO_FLAGS, "LD BC, d16" }
+    { 0x00, 0, 4, {NO_FLAGS}, "NOP" },
+    { 0x01, 2, 12, {NO_FLAGS}, "LD BC, d16" },
+    { 0x02, 0, 8, {NO_FLAGS}, "LD (BC), A" },
+    { 0x03, 0, 8, {NO_FLAGS}, "INC BC" },
+    { 0x04, 0, 4, {E_APPLIED, E_CLEARED, E_APPLIED, E_UNAFFECTED}, "INC B" },
+    { 0x05, 0, 4, {E_APPLIED, E_RAISED, E_APPLIED, E_UNAFFECTED}, "DEC B" },
+    { 0x06, 1, 8, {NO_FLAGS}, "LD B, d8" },
+    { 0x07, 0, 4, {E_CLEARED,E_CLEARED,E_CLEARED,E_APPLIED}, "RLCA" },
+    { 0x08, 2, 20, {NO_FLAGS}, "LD (a16), SP" },
+    { 0x09, 0, 8, {E_UNAFFECTED, E_CLEARED, E_APPLIED, E_APPLIED}, "ADD HL, BC" },
+    { 0x0a, 0, 8, {NO_FLAGS}, "LD A, (BC)" },
+    { 0x0b, 0, 8, {NO_FLAGS}, "DEC BC" },
+    { 0x0c, 0, 4, {E_APPLIED, E_CLEARED, E_APPLIED, E_UNAFFECTED}, "INC C" },
+    { 0x0d, 0, 4, {E_APPLIED, E_RAISED, E_APPLIED, E_UNAFFECTED}, "DEC C" },
+    { 0x0e, 1, 8, {NO_FLAGS}, "LD C, d8" },
+    { 0x0f, 0, 4, {E_CLEARED,E_CLEARED,E_CLEARED,E_APPLIED}, "RRCA" }
+
+
 };
 static instruction prefixCodeTable[ 0xFF ] = {
-    { 0x00,  0, 8, {FLAG_APPLIED, FLAG_CLEARED, FLAG_CLEARED, FLAG_APPLIED}, "RLC B" }
+    { 0x00,  0, 8, {E_APPLIED, E_CLEARED, E_CLEARED, E_APPLIED}, "RLC B" }
 };
