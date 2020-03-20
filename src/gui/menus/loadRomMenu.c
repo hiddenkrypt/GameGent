31291bad2afb72d3b7a4300dfc5760a22e60c373
Rav-T <hiddenkrypt@gmail.com>
2020-03-20 02:25:25 -0700

    new skeleton for the new menu system

--- src/gui/menus/loadRomMenu.c
+++ src/gui/menus/loadRomMenu.c
@@ -1,50 +1,16 @@
-//#include <SDL.h>
-//#include <string.h>
-//#include "../../../GameGent.h"
-//#include "../../dmg/dmg.h"
-//#include "../tileMap.h"
-//#include "menu.h"
-//#include "menuItem.h"
-//#include "loadRomMenu.h"
-//
-//static menuItem* menuItems;
-//static int menuItemCount = 0;
-//
-///** \brief get the numbered menuItem
-// * \return a copy of the menuItem struct for the numbered item
-// */
-//static menuItem getItem(int i){
-//    return menuItems[i];
-//}
-//
-///** \brief gets the number of menu items that are active
-// *
-// * generates a count of the number of menu items that return
-// * true when their activeCondition is called
-// * \return the count of items that are active
-// */
-//static int activeItems(){
-//    int count = 0;
-//    for( int i=0; i < menuItemCount; i++ ) {
-//        if ( menuItems[i].activeCondition() ) {
-//            count++;
-//        }
-//    }
-//    return count;
-//}
-///** \brief creates the Menu Struct for this menu
-// *
-// * The menu items list is dynamic, and should be generated here
-// * @todo populate menu item list when this function is called
-// * \return a menu struct for the load rom menu, with a list of menu items based on previously loaded roms
-// *
-// */
-//Menu LoadRomMenu_getMenu(){
-//    Menu loadRomMenu;
-//    loadRomMenu.itemCount = menuItemCount;
-//    loadRomMenu.activeItemCount = &activeItems;
-//    loadRomMenu.getItem = &getItem;
-//    return loadRomMenu;
-//}
-//
-//
+#include "menu.h"
+#include "menuItem.h"
+#include "loadRomMenu.h"
+
+static int itemCount(){ return 0; }
+static char* getLabel( int i ){ return ""; }
+static void activateItem(int i){}
+
+Menu LoadRomMenu_getMenu(){
+    Menu loadMenu;
+    loadMenu.itemCount = &itemCount;
+    loadMenu.getLabel = &getLabel;
+    loadMenu.activateItem = &activateItem;
+    return loadMenu;
+}
+
