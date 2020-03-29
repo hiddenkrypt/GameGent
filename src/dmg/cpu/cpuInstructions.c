3a009f7771f0819f636f55c9e68244a775128741
Rav-T <hiddenkrypt@gmail.com>
2020-03-28 21:46:50 -0700

    skeleton function calls in main decode switch done up through 0x8f, more than 50% through! the first half...

--- src/dmg/cpu/cpuInstructions.c
+++ src/dmg/cpu/cpuInstructions.c
@@ -5,22 +5,26 @@
 #include "cpuInstructions.h"
 void cpu_noop(){}
 void cpu_stop(){}
+void cpu_halt(){}
 void cpu_setCarryFlag(){}
 void cpu_flipCarryFlag(){}
 void load_16bitRegister_DirectWord( register16 targetRegister ){}
 void load_8bitRegister_DirectByte( register8 targetRegister ){}
-void load_8bitRegister_MemoryAtRegisterValue( register8 target, register16 address ){}
-void load_memoryAtRegisterValue_directByte( register16 address ){}
+void load_8bitRegister_MemoryAtRegisterValue( register8 targetRegister, register16 address ){}
+void load_8bitRegister_8bitRegister( register8 targetRegister, register8 dataRegister ){}
+void load_memory_directByte(){} //ld(HL)d8
 void load_memoryAtRegisterValue_8bitRegisterData( register16 address, register8 dataRegister ){}
-void load_memoryAtDirectWord_16bitRegisterData( register16 dataRegister ){}
+void load_memoryAtDirectWord_16bitRegister(){} //put SP in memory at direct addr
 void increment_16bitRegister( register16 targetRegister ){}
 void decrement_16bitRegister( register16 targetRegister ){}
 void increment_8bitRegister( register8 targetRegister ){}
 void decrement_8bitRegister( register8 targetRegister ){}
-void increment_memoryAtRegisterValue( register16 address){}
-void decrement_memoryAtRegisterValue( register16 address){}
+void increment_memoryValue(){} //inc(HL)
+void decrement_memoryValue(){} //dec(HL)
 void rotate_8bitRegister( register8 targetRegister, bool left, bool throughCarry ){}
-void add_16bitRegister_16bitRegister( register16 valueRegisterA, register16 valueRegisterB ){}
+void add_16bitRegister( register16 valueRegister ){}
+void add_8bitRegister( register8 valueRegister, bool carry ){}
+void add_memoryValue( bool carry ){} //a+=(HL)
 void accumulator_decimalAdjustment(){}
 void accumulator_complement(){}
 
@@ -51,10 +55,10 @@ inline void executeInstruction( instruction opcode ){
 			rotate_8bitRegister( A, true, false);
 			break;
 		case 0x08:
-			load_memoryAtDirectWord_16bitRegisterData( SP );
+			load_memoryAtDirectWord_16bitRegister(); //add sp
 			break;
 		case 0x09:
-			add_16bitRegister_16bitRegister( HL, BC );
+			add_16bitRegister( BC );
 			break;
 		case 0x0a:
 			load_8bitRegister_MemoryAtRegisterValue( A, BC );
@@ -101,7 +105,7 @@ inline void executeInstruction( instruction opcode ){
 		case 0x18:
 			/** @todo jump relative 8 bit data **/ break;
 		case 0x19:
-			add_16bitRegister_16bitRegister( HL, DE );
+			add_16bitRegister( DE );
 			break;
 		case 0x1a:
 			load_8bitRegister_MemoryAtRegisterValue( A, DE );
@@ -149,7 +153,7 @@ inline void executeInstruction( instruction opcode ){
 			/** @todo jump zero, relative 8 **/
 			break;
 		case 0x29:
-			add_16bitRegister_16bitRegister( HL, HL );
+			add_16bitRegister( HL );
 			break;
 		case 0x2a:
 			load_8bitRegister_MemoryAtRegisterValue( A, HL );
@@ -184,13 +188,13 @@ inline void executeInstruction( instruction opcode ){
 			increment_16bitRegister( SP );
 			break;
 		case 0x34:
-			increment_memoryAtRegisterValue( HL );
+			increment_memoryValue();
 			break;
 		case 0x35:
-			decrement_memoryAtRegisterValue( HL );
+			decrement_memoryValue();
 			break;
 		case 0x36:
-			load_memoryAtRegisterValue_directByte( HL );
+			load_memory_directByte();
 			break;
 		case 0x37:
 			cpu_setCarryFlag();
@@ -199,7 +203,7 @@ inline void executeInstruction( instruction opcode ){
 			/** @todo jump carry relative 8 **/
 			break;
 		case 0x39:
-			add_16bitRegister_16bitRegister( HL, SP );
+			add_16bitRegister( SP );
 			break;
 		case 0x3a:
 			load_8bitRegister_MemoryAtRegisterValue( A, HL );
@@ -221,244 +225,244 @@ inline void executeInstruction( instruction opcode ){
 			cpu_flipCarryFlag();
 			break;
 		case 0x40:
-
+			cpu_noop();
 			break;
 		case 0x41:
-
+			load_8bitRegister_8bitRegister( B, C );
 			break;
 		case 0x42:
-
+			load_8bitRegister_8bitRegister( B, D );
 			break;
 		case 0x43:
-
+			load_8bitRegister_8bitRegister( B, E );
 			break;
 		case 0x44:
-
+			load_8bitRegister_8bitRegister( B, H );
 			break;
 		case 0x45:
-
+			load_8bitRegister_8bitRegister( B, L );
 			break;
 		case 0x46:
-
+			load_8bitRegister_MemoryAtRegisterValue( B, HL );
 			break;
 		case 0x47:
-
+			load_8bitRegister_8bitRegister( B, A );
 			break;
 		case 0x48:
-
+			load_8bitRegister_8bitRegister( C, B );
 			break;
 		case 0x49:
-
+			cpu_noop();
 			break;
 		case 0x4a:
-
+			load_8bitRegister_8bitRegister( C, D );
 			break;
 		case 0x4b:
-
+			load_8bitRegister_8bitRegister( C, E );
 			break;
 		case 0x4c:
-
+			load_8bitRegister_8bitRegister( C, H );
 			break;
 		case 0x4d:
-
+			load_8bitRegister_8bitRegister( C, L );
 			break;
 		case 0x4e:
-
+			load_8bitRegister_MemoryAtRegisterValue( C, HL );
 			break;
 		case 0x4f:
-
+			load_8bitRegister_8bitRegister( C, A );
 			break;
 		case 0x50:
-
+			load_8bitRegister_8bitRegister( D, B );
 			break;
 		case 0x51:
-
+			load_8bitRegister_8bitRegister( D, C );
 			break;
 		case 0x52:
-
+			cpu_noop();
 			break;
 		case 0x53:
-
+			load_8bitRegister_8bitRegister( D, E );
 			break;
 		case 0x54:
-
+			load_8bitRegister_8bitRegister( D, H );
 			break;
 		case 0x55:
-
+			load_8bitRegister_8bitRegister( D, L );
 			break;
 		case 0x56:
-
+			load_8bitRegister_MemoryAtRegisterValue( D, HL );
 			break;
 		case 0x57:
-
+			load_8bitRegister_8bitRegister( D, A );
 			break;
 		case 0x58:
-
+			load_8bitRegister_8bitRegister( E, B );
 			break;
 		case 0x59:
-
+			load_8bitRegister_8bitRegister( E, C );
 			break;
 		case 0x5a:
-
+			load_8bitRegister_8bitRegister( E, D );
 			break;
 		case 0x5b:
-
+			cpu_noop();
 			break;
 		case 0x5c:
-
+			load_8bitRegister_8bitRegister( E, H );
 			break;
 		case 0x5d:
-
+			load_8bitRegister_8bitRegister( E, L );
 			break;
 		case 0x5e:
-
+			load_8bitRegister_MemoryAtRegisterValue( E, HL );
 			break;
 		case 0x5f:
-
+			load_8bitRegister_8bitRegister( E, A );
 			break;
 		case 0x60:
-
+			load_8bitRegister_8bitRegister( H, B );
 			break;
 		case 0x61:
-
+			load_8bitRegister_8bitRegister( H, C );
 			break;
 		case 0x62:
-
+			load_8bitRegister_8bitRegister( H, D );
 			break;
 		case 0x63:
-
+			load_8bitRegister_8bitRegister( H, E );
 			break;
 		case 0x64:
-
+			cpu_noop();
 			break;
 		case 0x65:
-
+			load_8bitRegister_8bitRegister( H, L );
 			break;
 		case 0x66:
-
+			load_8bitRegister_MemoryAtRegisterValue( H, HL );
 			break;
 		case 0x67:
-
+			load_8bitRegister_8bitRegister( H, A );
 			break;
 		case 0x68:
-
+			load_8bitRegister_8bitRegister( L, B );
 			break;
 		case 0x69:
-
+			load_8bitRegister_8bitRegister( L, C );
 			break;
 		case 0x6a:
-
+			load_8bitRegister_8bitRegister( L, D );
 			break;
 		case 0x6b:
-
+			load_8bitRegister_8bitRegister( L, E );
 			break;
 		case 0x6c:
-
+			load_8bitRegister_8bitRegister( L, H );
 			break;
 		case 0x6d:
-
+			cpu_noop();
 			break;
 		case 0x6e:
-
+			load_8bitRegister_MemoryAtRegisterValue( L, HL );
 			break;
 		case 0x6f:
-
+			load_8bitRegister_8bitRegister( L, A );
 			break;
 		case 0x70:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, B );
 			break;
 		case 0x71:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, C );
 			break;
 		case 0x72:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, D );
 			break;
 		case 0x73:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, E );
 			break;
 		case 0x74:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, H );
 			break;
 		case 0x75:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, L );
 			break;
 		case 0x76:
-
+			cpu_halt();
 			break;
 		case 0x77:
-
+			load_memoryAtRegisterValue_8bitRegisterData( HL, A );
 			break;
 		case 0x78:
-
+			load_8bitRegister_8bitRegister( A, B );
 			break;
 		case 0x79:
-
+			load_8bitRegister_8bitRegister( A, C );
 			break;
 		case 0x7a:
-
+			load_8bitRegister_8bitRegister( A, D );
 			break;
 		case 0x7b:
-
+			load_8bitRegister_8bitRegister( A, E );
 			break;
 		case 0x7c:
-
+			load_8bitRegister_8bitRegister( A, H );
 			break;
 		case 0x7d:
-
+			load_8bitRegister_8bitRegister( A, L );
 			break;
 		case 0x7e:
-
+			load_8bitRegister_MemoryAtRegisterValue( A, HL );
 			break;
 		case 0x7f:
-
+			cpu_noop();
 			break;
 		case 0x80:
-
+			add_8bitRegister( B, false );
 			break;
 		case 0x81:
-
+			add_8bitRegister( C, false );
 			break;
 		case 0x82:
-
+			add_8bitRegister( D, false );
 			break;
 		case 0x83:
-
+			add_8bitRegister( E, false );
 			break;
 		case 0x84:
-
+			add_8bitRegister( H, false );
 			break;
 		case 0x85:
-
+			add_8bitRegister( L, false );
 			break;
 		case 0x86:
-
+			add_memoryValue( false );
 			break;
 		case 0x87:
-
+			add_8bitRegister( A, false );
 			break;
 		case 0x88:
-
+			add_8bitRegister( B, true );
 			break;
 		case 0x89:
-
+			add_8bitRegister( C, true );
 			break;
 		case 0x8a:
-
+			add_8bitRegister( D, true );
 			break;
 		case 0x8b:
-
+			add_8bitRegister( E, true );
 			break;
 		case 0x8c:
-
+			add_8bitRegister( H, true );
 			break;
 		case 0x8d:
-
+			add_8bitRegister( L, true );
 			break;
 		case 0x8e:
-
+			add_memoryValue( true );
 			break;
 		case 0x8f:
-
+			add_8bitRegister( A, true );
 			break;
 		case 0x90:
 
